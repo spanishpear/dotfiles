2f84ad31c39cbdd7fb7534fb1148f3ee7b4b3cea
SpanishPear <shrey.somaiya@gmail.com>
2025-03-03 15:03:13 +1100

    new config, cleanup some stuff

---
+++ .config/nvim-lazy/plugin/after/themery.lua
@@ -0,0 +1,4 @@
+require("themery").setup({
+  themes = {"gruvbox", "dracula", "catppuccin", "catppuccin-frappe", "catppuccin-macchiato", "catppuccin-mocha", "tokyonight"}, -- Your list of installed colorschemes.
+  livePreview = true, -- Apply theme while picking. Default to true.
+})
